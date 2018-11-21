90baa8be3edc9bcdbeaabed0713a03fd63cf852f
Blak3Nick <blaken2021@gmail.com>
2018-11-21 14:00:34 -0500

    Added a uml file

--- src/blindsearch/BFS/Node.java
+++ src/blindsearch/BFS/Node.java
@@ -4,4 +4,14 @@ public class Node {
     private Object element;
     private boolean visited;
     private java.util.List<Node> neighbors;
+
+    public Node(Object object) {
+    }
+
+    public boolean isVisited() {
+        return false;
+    }
+
+    public void addNeighbors(Node node) {
+    }
 }
