271e2f68d9e088deacf8b9ccb2e56bb83493151d
Blak3Nick <blaken2021@gmail.com>
2018-11-22 05:19:43 -0500

    implementing the algorithm

--- src/blindsearch/BFS/webcrawler/BFS.java
+++ src/blindsearch/BFS/webcrawler/BFS.java
@@ -3,6 +3,8 @@ package blindsearch.BFS.webcrawler;
 import java.util.*;
 import java.net.*;
 import java.io.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class BFS {
@@ -19,7 +21,7 @@ public class BFS {
         this.urlPatterns = urlPatterns;
         this.repository = repository;
     }
-    public Set<String> bfs() {
+    public Set<String> bfs() throws IOException{
         repository.addURLtoSet(this.firstURL);
         while (repository.isQueueEmpty() == false) {
             String currentURL = repository.removeURLfromQUEUE();
@@ -27,23 +29,43 @@ public class BFS {
             if (repository.getUrlNo() > this.maximumURLNo) {
                 return this.repository.getUrlVisitedSet();
             }
-            boolean correctURL = false;
+            boolean correctURLFound = false;
             URL url = null;
             BufferedReader bufferedReader = null;
 
-            while ( correctURL == false) {
+            while ( correctURLFound == false) {
                 try {
                     url = new URL(currentURL);
                     try {
-                        bufferedReader = new BufferedReader(new InputStreamReader(url.openStream()));
+                         bufferedReader = new BufferedReader(new InputStreamReader(url.openStream()));
                     }catch (IOException e) {
+                        System.out.println("Error in buffered reader " + e.getMessage() + currentURL);
                         e.printStackTrace();
                     }
-
+                    correctURLFound = true;
                 } catch (MalformedURLException e) {
+                    System.out.println("Error in url " + e.getMessage() + currentURL);
                     e.printStackTrace();
                 }
 
+            }
+            StringBuilder stringBuilder = new StringBuilder();
+
+            while ((currentURL = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(currentURL);
+            }
+            currentURL= stringBuilder.toString();
+            Pattern pattern = Pattern.compile(this.urlPatterns);
+            Matcher matcher = pattern.matcher(currentURL);
+
+            while (matcher.find()) {
+                String urlString = matcher.group();
+
+                if (this.repository.setHasURL(urlString) == false) {
+                    this.repository.addURLtoSet(urlString);
+                    System.out.println(urlString);
+                    repository.addURLtoQueue(urlString);
+                }
             }
         }
         return null;
