4f7a5324773fcc40d75c751d4effdbe22b8df04d
Blak3Nick <blaken2021@gmail.com>
2018-11-22 04:55:00 -0500

    working on implementing the algorithm

--- src/blindsearch/BFS/webcrawler/Repository.java
+++ src/blindsearch/BFS/webcrawler/Repository.java
@@ -2,7 +2,13 @@ package blindsearch.BFS.webcrawler;
 
 import java.util.*;
 public class Repository {
-    private Queue<String> bfsQueue;
+    private Queue<String> bfsQueue ;
     private Set<String> urlVisitedSet;
     private int urlNo;
+
+    public Repository() {
+        this.bfsQueue =  new LinkedList<>();
+        this.urlVisitedSet = new HashSet<>();
+        this.urlNo = 0;
+    }
 }
