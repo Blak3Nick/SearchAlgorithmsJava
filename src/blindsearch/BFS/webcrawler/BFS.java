fae6ad6732a9586d99641d30a9b63fd939058887
Blak3Nick <blaken2021@gmail.com>
2018-11-22 05:05:56 -0500

    implementing the algorithm

--- src/blindsearch/BFS/webcrawler/BFS.java
+++ src/blindsearch/BFS/webcrawler/BFS.java
@@ -1,13 +1,34 @@
 package blindsearch.BFS.webcrawler;
 
-import java.util.Set;
+import java.util.*;
+import java.net.*;
+import java.io.*;
+
 
 public class BFS {
     private String firstURL;
     private int maximumURLNo;
     private String urlPatterns;
+    private Repository repository ;
 
+
+
+    public BFS(String firstURL, int maximumURLNo, String urlPatterns, Repository repository) {
+        this.firstURL = firstURL;
+        this.maximumURLNo = maximumURLNo;
+        this.urlPatterns = urlPatterns;
+        this.repository = repository;
+    }
     public Set<String> bfs() {
+        repository.addURLtoSet(this.firstURL);
+        while (repository.isQueueEmpty() == false) {
+            String currentURL = repository.removeURLfromQUEUE();
+
+            if (repository.getUrlNo() > this.maximumURLNo) {
+                return this.repository.getUrlVisitedSet();
+            }
+
+        }
         return null;
     }
 }
